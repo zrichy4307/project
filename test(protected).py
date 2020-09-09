5c9c6f6c37d1c6b54107cac59fe035a7f79a442b
zrichy4307 <65582886+zrichy4307@users.noreply.github.com>
2020-09-09 15:45:47 -0400

    Add files via upload

--- test(protected).py
+++ test(protected).py
@@ -1,15 +1,20 @@
 class Protected:
     def __init__(self):
         self.__privateVar = 24
+        self._protectedVar = 15
 
     def getPrivate(self):
         print(self.__privateVar)
+
+    def getProtected(self):
+        print(self._protectedVar)
 
     def setPrivate(self, private):
         self.__privateVar = private
 
 
 obj = Protected()
+obj.getProtected()
 obj.getPrivate()
 obj.setPrivate(23)
 obj.getPrivate()
