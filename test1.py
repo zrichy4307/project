7c57b27a3e1d2f94d96074fd4df71d49fbf516f9
zrichy4307 <65582886+zrichy4307@users.noreply.github.com>
2020-10-26 18:55:14 -0400

    Add files via upload

--- test1.py
+++ test1.py
@@ -9,45 +9,43 @@ with conn:
     cur.execute('DROP TABLE IF EXISTS tbl_files;')
     cur.execute('CREATE TABLE IF NOT EXISTS tbl_files (\
         ID INTEGER PRIMARY KEY AUTOINCREMENT, \
-        col_fname TEXT, \
-        col_ftype TEXT) ')
+        col_fname TEXT )')
         
     conn.commit()
 conn.close()
 
 
 
-
 conn = sqlite3.connect('fileList.db')
 
 
 with conn:
     cur = conn.cursor()
-    cur.execute('INSERT INTO tbl_files(col_fname, col_ftype) VALUES (?,?)' , \
-                ('information', 'docx'))
-    cur.execute('INSERT INTO tbl_files(col_fname, col_ftype) VALUES (?,?)' , \
-                ('Hello', 'txt'))
-    cur.execute('INSERT INTO tbl_files(col_fname, col_ftype) VALUES (?,?)' , \
-                ('myImage', 'png'))
-    cur.execute('INSERT INTO tbl_files(col_fname, col_ftype) VALUES (?,?)' , \
-                ('myMovie', 'mpg'))
-    cur.execute('INSERT INTO tbl_files(col_fname, col_ftype) VALUES (?,?)' , \
-                ('World', 'txt'))
-    cur.execute('INSERT INTO tbl_files(col_fname, col_ftype) VALUES (?,?)' , \
-                ('data', 'pdf'))
-    cur.execute('INSERT INTO tbl_files(col_fname, col_ftype) VALUES (?,?)' , \
-                ('myPhoto', 'jpg'))
+    cur.execute('INSERT INTO tbl_files(col_fname) VALUES ("Information.docx")')
+    cur.execute('INSERT INTO tbl_files(col_fname) VALUES ("Hello.txt")')
+    cur.execute('INSERT INTO tbl_files(col_fname) VALUES ("myImage.png")')
+    cur.execute('INSERT INTO tbl_files(col_fname) VALUES ("myMovie.mpg")')
+    cur.execute('INSERT INTO tbl_files(col_fname) VALUES ("World.txt")')
+    cur.execute('INSERT INTO tbl_files(col_fname) VALUES ("data.pdf")')
+    cur.execute('INSERT INTO tbl_files(col_fname) VALUES ("myPhoto.jpg")')
+                
+   
     
     conn.commit()
 conn.close()
 
-conn = sqlite3.connect('fileList.db')
 
+conn = sqlite3.connect('fileList.db')
 
 with conn:
     cur = conn.cursor()
-    cur.execute('SELECT col_fname FROM tbl_files WHERE col_ftype = "txt"')
+    cur.execute('Select * FROM tbl_files WHERE col_fname LIKE "%txt"')
     varFile = cur.fetchall()
     for item in varFile:
-        items = "Your txt files are displayed here: {}.txt".format(item[0] + '{}.txt'.format(item[0]))
-    print(items)
+        msg = "Files you've selected are: {} {}".format(item[0], item[1])
+        print(msg)
+
+
+
+
+
