d7d06a4af45688d72a61166c357d224ebc2ca34e
Zachery Richmond <65582886+zrichy4307@users.noreply.github.com>
2020-10-28 23:14:18 -0400

    Add files via upload

--- test1.py
+++ test1.py
@@ -16,23 +16,11 @@ conn.close()
 
 
 
-conn = sqlite3.connect('fileList.db')
-
-
-with conn:
-    cur = conn.cursor()
-    cur.execute('INSERT INTO tbl_files(col_fname) VALUES ("Information.docx")')
-    cur.execute('INSERT INTO tbl_files(col_fname) VALUES ("Hello.txt")')
-    cur.execute('INSERT INTO tbl_files(col_fname) VALUES ("myImage.png")')
-    cur.execute('INSERT INTO tbl_files(col_fname) VALUES ("myMovie.mpg")')
-    cur.execute('INSERT INTO tbl_files(col_fname) VALUES ("World.txt")')
-    cur.execute('INSERT INTO tbl_files(col_fname) VALUES ("data.pdf")')
-    cur.execute('INSERT INTO tbl_files(col_fname) VALUES ("myPhoto.jpg")')
-                
-   
-    
-    conn.commit()
-conn.close()
+fileList = ('information.docx', 'Hello.txt', 'myImage.png', \
+            'myMovie.mpg', 'World.txt', 'data.pdf', 'myPhoto.jpg')
+for i in fileList:
+    if i.endswith('.txt'):
+        print(i)
 
 
 conn = sqlite3.connect('fileList.db')
@@ -40,10 +28,15 @@ conn = sqlite3.connect('fileList.db')
 with conn:
     cur = conn.cursor()
     cur.execute('Select * FROM tbl_files WHERE col_fname LIKE "%txt"')
-    varFile = cur.fetchall()
-    for item in varFile:
-        msg = "Files you've selected are: {} {}".format(item[0], item[1])
-        print(msg)
+    cur.execute('''
+                INSERT INTO fileList.db.tbl_files(?)
+                VALUES
+                (i)
+                ''')
+    conn.commit()
+conn.close()
+
+
 
 
 
