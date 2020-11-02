f51a9a3aa3a6bad18268e8bef93744095340fcbc
Zachery Richmond <65582886+zrichy4307@users.noreply.github.com>
2020-11-02 14:37:34 -0500

    Add files via upload

--- FileTransfer.py
+++ FileTransfer.py
@@ -1,27 +1,81 @@
-import time
+import tkinter
+from tkinter import *
+from tkinter import filedialog
 import os
 import shutil
-import tkinter
-from tkinter import Tk     # from tkinter import Tk for Python 3.x
-from tkinter.filedialog import askopenfilename
 
-Tk().withdraw() # we don't want a full GUI, so keep the root window from appearing
-filename = askopenfilename() # show an "Open" dialog box and return the path to the selected file
-print(filename)
 
-SECONDS_IN_DAY = 24 * 60 * 60
 
-src = "C:\\Users\\Zach\\Desktop\\FolderA"
-dst = "C:\\Users\\Zach\\Desktop\\FolderB/"
 
-now = time.time()
-before = now - SECONDS_IN_DAY
+class ParentWindow(Frame):
+    def __init__ (self, master):
+        Frame.__init__ (self)
 
-def last_mod_time(fname):
-    return os.path.getmtime(fname)
+        self.master = master
+        self.master.resizable(width=False, height=True)
+        self.master.geometry('{}x{}'.format(700,400))
+        self.master.title('File Transfer!')
+        self.master.config(bg='lightgray')
 
-for fname in os.listdir(src):
-    src_fname = os.path.join(src, fname)
-    if last_mod_time(src_fname) > before:
-        dst_fname = os.path.join(dst, fname)
-        shutil.move(src_fname, dst_fname)
+        self.sourcePath =  StringVar()
+        self.destPath =  StringVar()
+        
+        self.btnsourcePath = Button(self.master,text='Source Folder: ', font =("Helvetica",16), fg='black', bg='lightgray' )
+        self.btnsourcePath.grid(row=0, column=0,padx=(30,0),pady=(30,0))
+        
+        self.btndestPath = Button(self.master,text='Destination Folder: ', font =("Helvetica",16), fg='black', bg='lightgray' )
+        self.btndestPath.grid(row=1, column=0,padx=(30,0),pady=(30,0))
+
+        self.txtSource = Entry(self.master,text=self.sourcePath, font=("Helvetica",16), fg='black', bg='lightgray' )
+        self.txtSource.grid(row=0, column=1,padx=(30,0),pady=(30,0))
+
+        self.txtDestination = Entry(self.master,text=self.sourcePath, font=("Helvetica",16), fg='black', bg='lightgray' )
+        self.txtDestination.grid(row=1, column=1,padx=(30,0),pady=(30,0))
+
+        self.lblDisplay = Label(self.master,text='', font =("Helvetica",16), fg='black', bg='lightgray' )
+        self.lblDisplay.grid(row=3,column=1,padx=(30,0),pady=(30,0))
+
+        self.txtsourcePath = Entry(self.master,text=self.sourcePath, font=('Helvitica',16),fg='black', bg='lightblue')
+        self.txtsourcePath.grid(row=0,column=1,padx=(30,0),pady=(30,0))
+        
+        self.txtdestPath = Entry(self.master,text=self.destPath, font=('Helvitica',16),fg='black', bg='lightblue')
+        self.txtdestPath.grid(row=1,column=1,padx=(30,0),pady=(30,0))
+
+        self.btnSource = Button(self.master, text='Source Folder: ', font=('Helvitica',16),fg='black', bg='lightblue', command=self.getSource)
+        self.btnSource.grid(row=0,column=0,padx=(30,0),pady=(30,0))
+
+        self.btnDestination = Button(self.master, text='Destination Folder: ', font=('Helvitica',16),fg='black', bg='lightblue', command=self.getDestination)
+        self.btnDestination.grid(row=1,column=0,padx=(30,0),pady=(30,0))
+
+        self.btnTransfer = Button(self.master, text='Transfer', width=10, height=2, command=self.transfer)
+        self.btnTransfer.grid(row=3,column=2,padx=(0,90),pady=(30,0),sticky=NE)
+
+
+    def transfer(self):
+        self.master.destroy()
+
+    def getSource(self):
+        srcPath = filedialog.askdirectory()
+        self.txtSource.delete(0,END)
+        self.txtSource.insert(0, srcPath)
+
+    def getDestination(self):
+        dstPath = filedialog.askdirectory()
+        self.txtDestination.delete(0,END)
+        self.txtDestination.insert(0, dstPath)
+        
+        
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    root = Tk()
+    App = ParentWindow(root)
+    root.mainloop()
